4fbdaedeb287fd42b6c968e211a455a82da5969b
vinod <vnd1305@gmail.com>
2024-01-29 23:11:10 +0530

    sanity reports

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 
 # Run your Python script
-pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+#pytest -s -v -m "sanity" --html=./Reports/sanityCases.html testCases/ --browser chrome
+
+pytest -s -v -m "sanity" --html=./Reports/sanityCases.html testCases/ --browser firefox
+
