bc57f912186df962daa16636321fbee2aa360610
Vinod Joel <81827633+vndjoelb96@users.noreply.github.com>
2024-01-30 22:35:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 # Run your Python script
-#pytest -s -v -m "sanity" --html=./Reports/sanityCases.html testCases/ --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/sanityCases.html testCases/ --browser chrome
 
-pytest -s -v -m "regression" --html=./Reports/regressionCases.html testCases/ --browser firefox
+#pytest -s -v -m "regression" --html=./Reports/regressionCases.html testCases/ --browser firefox
 
